a0e464c7c5d525950325f9a21c96a12e779a01a9
kpping <kp@kpping.me>
2019-09-26 16:29:33 +0700

    re-compiled

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,4 +1,6 @@
-import { NextFunction, Request, RequestHandler, Response } from 'express';
+import { NextFunction, Request, RequestHandler as ExpressRequestHandler, Response } from 'express';
+import { RequestHandler as ExpressServeStaticCoreRequestHandler } from 'express-serve-static-core';
+export declare type RequestHandler = ExpressRequestHandler | ExpressServeStaticCoreRequestHandler;
 export declare function makeIteratorFromList<T>(list: T[]): IterableIterator<T>;
 export declare function assert3ParamsType(middlewareList: RequestHandler[]): boolean;
 export declare function middlewareRunner(middlewareList: RequestHandler[], req: Request, res: Response, next: NextFunction): void;
