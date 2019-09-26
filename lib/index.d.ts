83693c1531864812c19f70c119559233060aac32
kpping <kp@kpping.me>
2019-09-26 16:42:39 +0700

    re-compiled

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,7 +1,8 @@
-import { NextFunction, Request, RequestHandler as ExpressRequestHandler, Response } from 'express';
-import { RequestHandler as ExpressServeStaticCoreRequestHandler } from 'express-serve-static-core';
-export declare type RequestHandler = ExpressRequestHandler | ExpressServeStaticCoreRequestHandler;
+import { NextFunction, Request as ExpressRequest, RequestHandler as ExpressRequestHandler, Response } from 'express';
+import { RequestHandler as ExpressServeStaticCoreRequestHandler, Params, ParamsDictionary, Request as ExpressServeStaticCoreRequest } from 'express-serve-static-core';
+export declare type Request<T extends Params = ParamsDictionary> = ExpressRequest | ExpressServeStaticCoreRequest<T>;
+export declare type RequestHandler<T extends Params = ParamsDictionary> = ExpressRequestHandler | ExpressServeStaticCoreRequestHandler<T>;
 export declare function makeIteratorFromList<T>(list: T[]): IterableIterator<T>;
-export declare function assert3ParamsType(middlewareList: RequestHandler[]): boolean;
-export declare function middlewareRunner(middlewareList: RequestHandler[], req: Request, res: Response, next: NextFunction): void;
+export declare function assert3ParamsType<T extends Params = ParamsDictionary>(middlewareList: RequestHandler<T>[]): boolean;
+export declare function middlewareRunner<T extends Params = ParamsDictionary>(middlewareList: RequestHandler<T>[], req: Request, res: Response, next: NextFunction): void;
 //# sourceMappingURL=index.d.ts.map
